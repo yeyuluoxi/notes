d90f7ed6601d0f937e903da377cc85c86aba0471
wangtao <1477017843@qq.com>
2021-03-12 16:40:56 +0800

    note

--- src/data/daily/March/twelfth.ts
+++ src/data/daily/March/twelfth.ts
@@ -2,8 +2,9 @@ import {IDaily, IType, month, date } from "@/data/daily/base";
 
 const first: IDaily = {
     date: [ month.March, date.Twelfth ],
-    type: IType.Js,
-    note: ``
+    type: IType.Css,
+    note: `/deep/ : 样式穿透~
+        pointer-events: none; 点击穿透`
 }
 
 const MarchTwelfth: Array<IDaily> = [ first ];
